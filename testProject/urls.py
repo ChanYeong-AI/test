d973b86c77a27e4930f4a4fbfc452f1b36aee640
ChanyeongPark <albam6912@naver.com>
2020-09-01 09:55:13 +0900

    add page_3

--- testProject/urls.py
+++ testProject/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('page_1', views.page_1, name='page_1'),
-    path('page_2', views.page_2, name='page_2')
+    path('page_2', views.page_2, name='page_2'),
+    path('page_3', views.page_3, name='page_3')
 ]
